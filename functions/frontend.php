d978cd61a470bdff2d1bf9fef08a977cf8f3fb09
UmmeHabiba-7119 <habiba15-7119@diu.edu.bd>
2020-10-10 10:58:36 -0700

    First Commit

---
+++ functions/frontend.php
@@ -0,0 +1,13 @@
+<?php
+
+
+  /*****************************************************************************
+  This page maintain all the function for the frontend
+  *****************************************************************************/
+
+  //page joining class
+  require_once 'Classes/pageContent.php';
+
+
+  //object of pageContent class
+  $page_content = new pageContent();
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  require_once('functions/frontend.php');
+
+  $page_content->getIndexHeader();
+  $page_content->getContent('slider');
+  $page_content->getContent('tranding');
+  $page_content->getContent('section1');
+  $page_content->getContent('newspost');
+  $page_content->getContent('section2');
+  $page_content->getFooter();
